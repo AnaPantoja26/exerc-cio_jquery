69c7a58beaa480e0a3ce0b06c75331bbafca0444
Ana Camila Pantoja <anacamilapantoja26@gmail.com>
2024-04-24 13:28:58 -0300

    primeiro commit

--- main.js
+++ main.js
@@ -1,9 +1,43 @@
-$('form').on('submit', function(e) {
+    const form = document.getElementById('form-lista');
+    const novaTarefa = [];
+    let linhas = '';
+
+    form.addEventListener('submit', function(e) {
         e.preventDefault();
 
-        const tarefa = $('#tarefa-nova').val();
-        const novoItem = $('<li style="display: none"><li/>');
-        
-        $(`<a href= "${tarefa}"</a>`).appendTo(novoItem);
-        $(novoItem).appendTo('ul');
+    addTarefa();
+    atualizaTarefa();
+
+    })
+
+    function addTarefa() {
+        const inputTarefa = document.getElementById('tarefa-nova');
+
+        if(novaTarefa.includes(inputTarefa.value)) {
+            alert(`a tarefa ${inputTarefa.value} já foi inserida!`);
+        }else {
+            novaTarefa.push(inputTarefa.value);
+
+            let linha = `<ul>`;
+            linha +=`<li> ${inputTarefa.value}`;
+            linha +=`</ul>`;
+
+            linhas += linha;
+        }
+
+    inputTarefa.value = '';
+    }
+
+    function atualizaTarefa() {
+        const tabelaMatriz = document.querySelector('ul');
+        tabelaMatriz.innerHTML = linhas;
+    }
+
+    const tabelaMatriz = document.querySelector('ul');
+    tabelaMatriz.addEventListener('click', function(e){
+        if (e.target.classList.contains('checked')){
+            e.target.classList.remove('checked');
+        }else{
+            e.target.classList.add('checked');
+        }
     })
